167ff3231e670599430f01a53ae2c565b1158b3e
TomYoYo <tom.pueyo@gmail.com>
2015-11-08 15:04:07 +0100

    Balls

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine; 
+using System.Collections; 
+
+public class Control : MonoBehaviour {  
+	public int speed = 2;  
+	private Rigidbody rb; 
+	
+	void Start()  
+	{  
+		rb = GetComponent<Rigidbody>();  
+	} 
+	void Update()  
+	{  
+		KeyboardMovements();  
+	} 
+	
+	void KeyboardMovements()  
+	{  
+		if(Input.GetKey("right"))  
+		{  
+			rb.AddForce(Vector3.right * speed);  
+		}  
+		else if(Input.GetKey("left"))  
+		{  
+			rb.AddForce(Vector3.left * speed);  
+		} 
+		else if(Input.GetKey("up"))  
+		{  
+			rb.AddForce(Vector3.forward * speed);  
+		}  
+		else if(Input.GetKey("down"))  
+		{  
+			rb.AddForce(Vector3.back * speed);  
+		}  
+	}  
+} 
