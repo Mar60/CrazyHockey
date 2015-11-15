bbd3432e9e63238d4c4c526b69c26eac245ca83c
TomYoYo <tom.pueyo@gmail.com>
2015-11-15 14:31:07 +0100

    Test

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        Application.LoadLevel(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    void OnMouseUp()
+    {
+        Debug.Log("lol");
+    }
+}
