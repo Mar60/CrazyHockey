95b48a5f9e3f2d9e3600cf8386bde9f2fbdfd2d3
Mar60 <hammi.marouane@gmail.com>
2015-11-20 08:13:10 +0100

    [Mar] raycasting start with previous version and balltrigger also

---
+++ Assets/Scripts/BallTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallTrigger : MonoBehaviour {
+    private GameObject lastPlayerTouched;
+    public BonusGenerator manager;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collision collideEvent)
+    {
+        if (collideEvent.gameObject.CompareTag("BonusShield"))
+        {
+            Debug.Log("Destruction bonus shield");
+            Destroy(collideEvent.gameObject);
+            manager.BonusDestroyed();
+        }
+    }
+}
